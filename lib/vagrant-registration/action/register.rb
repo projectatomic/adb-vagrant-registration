d65ccc10e393ce65a8812e96add86be4c8a3cb52
Josef Stribny <jstribny@redhat.com>
2014-10-23 10:03:52 +0200

    Call the guest capability correctly

--- lib/vagrant-registration/action/register.rb
+++ lib/vagrant-registration/action/register.rb
@@ -1,20 +1,23 @@
 module VagrantPlugins
   module Registration
     module Action
-      # This unregisters the guest if the guest plugin supports it
+      # This registers the guest if the guest plugin supports it
       class Register
 
         def initialize(app, env)
           @app    = app
+          @env    = env
           @logger = Log4r::Logger.new("vagrant_register::action::register")
         end
 
         def call(env)
           @app.call(env)
+          guest = @env[:machine].guest
           @logger.info("Testing for registration_register capability on ")
-          if @machine.guest.capability?(:registration_register)
+
+          if guest.capability?(:registration_register)
             @logger.info("registration_register capability exists on ")
-            result = @machine.guest.capability(:registration_register)
+            result = guest.capability(:registration_register)
             @logger.info("called registration_register capability on ")
           end
         end
