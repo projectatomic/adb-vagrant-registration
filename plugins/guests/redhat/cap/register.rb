9df3ab6d013f4e0ce905fe9977640507c0fad21b
Josef Stribny <jstribny@redhat.com>
2014-10-29 15:15:19 +0100

    Skip registration if the system is already registered

--- plugins/guests/redhat/cap/register.rb
+++ plugins/guests/redhat/cap/register.rb
@@ -5,7 +5,8 @@ module VagrantPlugins
         def self.register(machine)
           username = machine.config.registration.subscriber_username
           password = machine.config.registration.subscriber_password	       
-          machine.communicate.execute("subscription-manager register --username=#{username} --password=#{password} --auto-attach", sudo: true)
+          command = "subscription-manager register --username=#{username} --password=#{password} --auto-attach"
+          machine.communicate.execute("cmd=$(#{command}); if [ \"$?\" != \"0\" ]; then echo $cmd | grep 'This system is already registered' || (echo $cmd 1>&2 && exit 1) ; fi", sudo: true)
         rescue IOError
         # Ignore, this probably means connection closed because it
         # shut down.
