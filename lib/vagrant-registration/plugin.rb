8a96ba09a0f15b3dca1829ae3aaa950f40e45552
Josef Stribny <jstribny@redhat.com>
2015-01-12 10:07:52 +0100

    Fix logging

--- lib/vagrant-registration/plugin.rb
+++ lib/vagrant-registration/plugin.rb
@@ -9,18 +9,18 @@ module VagrantPlugins
     class Plugin < Vagrant.plugin("2")
       class << self
         def initialize(app, env)
+          setup_logging
           @app    = app
-          @logger = Log4r::Logger.new("vagrant_registration::plugin")
         end
 
         def register(hook)
-          @logger.info("in register hook")
+          setup_logging
           hook.after(::Vagrant::Action::Builtin::SyncedFolders,
                      VagrantPlugins::Registration::Action.action_register)
         end
 
         def unregister(hook)
-          @logger.info("in unregister hook")
+          setup_logging
           hook.prepend(VagrantPlugins::Registration::Action.action_unregister)
         end
       end
@@ -31,12 +31,8 @@ module VagrantPlugins
       support the capability
       DESC
 
-      @logger = Log4r::Logger.new("vagrant_registration::plugin::setup")
-      @logger.info("attempting to register hooks on up and provision")
       action_hook(:registration_register, :machine_action_up, &method(:register))
       action_hook(:registration_register, :machine_action_provision, &method(:register))
-
-      @logger.info("attempting to register hooks on halt and destroy")
       action_hook(:registration_unregister, :machine_action_halt, &method(:unregister))
       action_hook(:registration_unregister, :machine_action_destroy, &method(:unregister))
 
@@ -45,6 +41,32 @@ module VagrantPlugins
         Config
       end
 
+      # This sets up our log level to be whatever VAGRANT_LOG is
+      # for loggers prepended with 'vagrant_libvirt'
+      def self.setup_logging
+        require 'log4r'
+        level = nil
+        begin
+          level = Log4r.const_get(ENV['VAGRANT_LOG'].upcase)
+        rescue NameError
+          # This means that the logging constant wasn't found,
+          # which is fine. We just keep `level` as `nil`. But
+          # we tell the user.
+          level = nil
+        end
+        # Some constants, such as "true" resolve to booleans, so the
+        # above error checking doesn't catch it. This will check to make
+        # sure that the log level is an integer, as Log4r requires.
+        level = nil if !level.is_a?(Integer)
+        # Set the logging level on all "vagrant" namespaced
+        # logs as long as we have a valid level.
+        if level
+          logger = Log4r::Logger.new('vagrant_registration')
+          logger.outputters = Log4r::Outputter.stderr
+          logger.level = level
+          logger = nil
+        end
+      end
     end
   end
 end
