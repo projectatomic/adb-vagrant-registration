089ce4d36ccce1855ed25d8b4b6d9d4224ab4477
Tomáš Hrčka <thrcka@redhat.com>
2015-01-15 13:36:55 +0100

    Tweaks to fedora guest

---
+++ plugins/guests/fedora/cap/flavor.rb
@@ -0,0 +1,24 @@
+module VagrantPlugins
+  module GuestFedora
+    module Cap
+      class Flavor
+        def self.flavor(machine)
+          # Read the version file
+          output = ""
+          machine.communicate.sudo("grep VERSION_ID /etc/os-release") do |type, data|
+            version = data.split("=")[1].chomp.to_i if type == :stdout
+          end
+
+          # Detect various flavors we care about
+          if version >= 20
+            return :"fedora_#{version}"
+          else if version <= 16
+            return :"fedora_#{version}"
+          else
+            return :fedora
+          end
+        end
+      end
+    end
+  end
+end
