edd413483cbe45ef90361e4c9e807b68a917d3af
Tomas Hrcka <thrcka@redhat.com>
2015-02-09 09:01:07 +0100

    Add force config option to register capability

--- lib/vagrant-registration/config.rb
+++ lib/vagrant-registration/config.rb
@@ -18,16 +18,23 @@ module VagrantPlugins
       # @return [Bool]
       attr_accessor :skip
 
+      # Force the registration (skip if true)
+      #
+      # @return [Bool]
+      attr_accessor :force
+
       def initialize(region_specific=false)
         @subscriber_username = UNSET_VALUE
         @subscriber_password = UNSET_VALUE
         @skip = UNSET_VALUE
+	@force = UNSET_VALUE
       end
 
       def finalize!
         @subscriber_username = nil if @subscriber_username == UNSET_VALUE
         @subscriber_password = nil if @subscriber_password == UNSET_VALUE
         @skip = false if @skip == UNSET_VALUE
+	@force = false if @force == UNSET_VALUE
       end
     end
   end
--- plugins/guests/redhat/cap/register.rb
+++ plugins/guests/redhat/cap/register.rb
@@ -4,8 +4,8 @@ module VagrantPlugins
       class Register
         def self.register(machine)
           username = machine.config.registration.subscriber_username
-          password = machine.config.registration.subscriber_password	       
-          command = "subscription-manager register --username=#{username} --password=#{password} --auto-attach"
+          password = machine.config.registration.subscriber_password
+          command = "subscription-manager register --username=#{username} --password=#{password} --auto-attach #{"--force" if machine.config.registration.force}"
           machine.communicate.execute("cmd=$(#{command}); if [ \"$?\" != \"0\" ]; then echo $cmd | grep 'This system is already registered' || (echo $cmd 1>&2 && exit 1) ; fi", sudo: true)
         end
       end
