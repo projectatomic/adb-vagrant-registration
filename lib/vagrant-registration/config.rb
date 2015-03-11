75664c750ad69145106f03f65b552ced970f0634
Daniel Tschan <tschan@puzzle.ch>
2015-03-11 13:38:03 +0100

    Add support for subscription-manager register options --auto-attach and --name.

--- lib/vagrant-registration/config.rb
+++ lib/vagrant-registration/config.rb
@@ -18,16 +18,28 @@ module VagrantPlugins
       # @return [Bool]
       attr_accessor :skip
 
-      # Force the registration (skip if true)
+      # Force the registration (force if true)
       #
       # @return [Bool]
       attr_accessor :force
+
+      # Auto attach suitable subscriptions (auto attach if true)
+      #
+      # @return [Bool]
+      attr_accessor :auto_attach
+
+      # Name of the subscribed system (optional, defaults to hostname if unset)
+      #
+      # @return [String]
+      attr_accessor :name
 
       def initialize(region_specific=false)
         @subscriber_username = UNSET_VALUE
         @subscriber_password = UNSET_VALUE
         @skip = UNSET_VALUE
         @force = true
+        @auto_attach = true
+        @name = UNSET_VALUE
       end
 
       def finalize!
@@ -35,6 +47,8 @@ module VagrantPlugins
         @subscriber_password = nil if @subscriber_password == UNSET_VALUE
         @skip = false if @skip == UNSET_VALUE
         @force = true if @force == UNSET_VALUE
+        @auto_attach = true if @auto_attach == UNSET_VALUE
+        @name = nil if @name == UNSET_VALUE
       end
     end
   end
