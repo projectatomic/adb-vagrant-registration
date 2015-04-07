50f0b3444d6ed3a126d15c4de1d274783f6b2243
Josef Stribny <jstribny@redhat.com>
2015-04-07 12:54:52 +0200

    Implement all subscription-manager register options

--- lib/vagrant-registration/config.rb
+++ lib/vagrant-registration/config.rb
@@ -3,52 +3,113 @@ require "vagrant"
 module VagrantPlugins
   module Registration
     class Config < Vagrant.plugin("2", :config)
-      # The username to subscribe with
+      # The username to subscribe with (required)
       #
       # @return [String]
       attr_accessor :subscriber_username
 
-      # The password of the subscriber
+      # The password of the subscriber (required)
       #
       # @return [String]
       attr_accessor :subscriber_password
 
-      # Skip the registration (skip if true)
+      # Give the hostname of the subscription service to use (required for Subscription
+      # Asset Manager, defaults to Customer Portal Subscription Management)
       #
-      # @return [Bool]
-      attr_accessor :skip
+      # @return [String]
+      attr_accessor :serverurl
 
-      # Force the registration (force if true)
+      # Give the hostname of the content delivery server to use to receive updates
+      # (required for Satellite 6)
       #
-      # @return [Bool]
-      attr_accessor :force
+      # @return [String]
+      attr_accessor :baseurl
 
-      # Auto attach suitable subscriptions (auto attach if true)
+      # Give the organization to which to join the system (required, except for
+      # hosted environments)
       #
-      # @return [Bool]
-      attr_accessor :auto_attach
+      # @return [String]
+      attr_accessor :org
+
+      # Register the system to an environment within an organization (optional)
+      #
+      # @return [String]
+      attr_accessor :environment
 
       # Name of the subscribed system (optional, defaults to hostname if unset)
       #
       # @return [String]
       attr_accessor :name
 
+      # Auto attach suitable subscriptions (optional, auto attach if true)
+      #
+      # @return [Bool]
+      attr_accessor :auto_attach
+
+      # Attach existing subscriptions as part of the registration process (optional)
+      #
+      # @return [String]
+      attr_accessor :activationkey
+
+      # Set the service level to use for subscriptions on that machine
+      # (optional, used only used with the --auto-attach)
+      #
+      # @return [String]
+      attr_accessor :servicelevel
+
+      # Set the operating system minor release to use for subscriptions for
+      # the system (optional, used only used with the --auto-attach)
+      #
+      # @return [String]
+      attr_accessor :release
+
+      # Force the registration (optional, force if true, defaults to true)
+      #
+      # @return [Bool]
+      attr_accessor :force
+
+      # Set what type of consumer is being registered (optional, defaults to system)
+      #
+      # @return [String]
+      attr_accessor :type
+
+      # Skip the registration (optional, skip if true, defaults to false)
+      #
+      # @return [Bool]
+      attr_accessor :skip
+
       def initialize(region_specific=false)
         @subscriber_username = UNSET_VALUE
         @subscriber_password = UNSET_VALUE
-        @skip = UNSET_VALUE
-        @force = true
-        @auto_attach = true
+        @serverurl = UNSET_VALUE
+        @baseurl = UNSET_VALUE
+        @org = UNSET_VALUE
+        @environment = UNSET_VALUE
         @name = UNSET_VALUE
+        @auto_attach = true
+        @activationkey = UNSET_VALUE
+        @servicelevel = UNSET_VALUE
+        @release = UNSET_VALUE
+        @force = true
+        @type = UNSET_VALUE
+        @skip = UNSET_VALUE
       end
 
       def finalize!
         @subscriber_username = nil if @subscriber_username == UNSET_VALUE
         @subscriber_password = nil if @subscriber_password == UNSET_VALUE
-        @skip = false if @skip == UNSET_VALUE
-        @force = true if @force == UNSET_VALUE
-        @auto_attach = true if @auto_attach == UNSET_VALUE
+        @serverurl = nil if @serverurl = UNSET_VALUE
+        @baseurl = nil if @baseurl = UNSET_VALUE
+        @org = nil if @org = UNSET_VALUE
+        @environment = nil if @environment = UNSET_VALUE
         @name = nil if @name == UNSET_VALUE
+        @auto_attach = true if @auto_attach == UNSET_VALUE
+        @activationkey = nil if @activationkey = UNSET_VALUE
+        @servicelevel = nil if @servicelevel = UNSET_VALUE
+        @release = nil if @release = UNSET_VALUE
+        @force = true if @force == UNSET_VALUE
+        @type = nil if @type == UNSET_VALUE
+        @skip = false if @skip == UNSET_VALUE
       end
     end
   end
--- plugins/guests/redhat/cap/register.rb
+++ plugins/guests/redhat/cap/register.rb
@@ -3,10 +3,29 @@ module VagrantPlugins
     module Cap
       class Register
         def self.register(machine)
-          username = machine.config.registration.subscriber_username
-          password = machine.config.registration.subscriber_password
-          command = "subscription-manager register --username=#{username} --password=#{password} #{"--name=#{machine.config.registration.name}" if machine.config.registration.name} #{"--auto-attach" if machine.config.registration.auto_attach} #{"--force" if machine.config.registration.force}"
+          command = "subscription-manager register #{configuration_to_options(machine.config)}"
           machine.communicate.execute("cmd=$(#{command}); if [ \"$?\" != \"0\" ]; then echo $cmd | grep 'This system is already registered' || (echo $cmd 1>&2 && exit 1) ; fi", sudo: true)
+        end
+
+        private
+
+        # Build additional subscription-manager options based on plugin configuration
+        def self.configuration_to_options(config)
+          options = []
+          options << "--username=#{config.registration.subscriber_username}"
+          options << "--password=#{config.registration.subscriber_password}"
+          options << "--serverurl=#{config.registration.serverurl}" if config.registration.serverurl
+          options << "--baseurl=#{config.registration.baseurl}" if config.registration.baseurl
+          options << "--org=#{config.registration.org}" if config.registration.org
+          options << "--environment=#{config.registration.environment}" if config.registration.environment
+          options << "--name=#{config.registration.name}" if config.registration.name
+          options << "--auto-attach" if config.registration.auto_attach
+          options << "--activationkey=#{config.registration.activationkey}" if config.registration.activationkey
+          options << "--servicelevel=#{config.registration.servicelevel}" if config.registration.servicelevel
+          options << "--release=#{config.registration.release}" if config.registration.release
+          options << "--force" if config.registration.force
+          options << "--type=#{config.registration.type}" if config.registration.type
+          options.join(' ')
         end
       end
     end
